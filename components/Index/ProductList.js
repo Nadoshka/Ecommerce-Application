d9fc95cba323cd08324eb0e1a2d75cf70c15f9e5
Nadin Atik <funatik@Nadoshka.local>
2020-10-14 22:29:18 +0200

    updates

--- components/Index/ProductList.js
+++ components/Index/ProductList.js
@@ -4,7 +4,8 @@ import { Card } from "semantic-ui-react";
    function mapProductsToItems(products) {
      return products.map(product => ({
       header: product.name,
-      image: product.mediaUrl,       meta: `$${product.price}`,
+      image: product.mediaUrl, 
+      meta: `$${product.price}`,
       color: "teal",
       fluid: true,
       childKey: product._id,
--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   env: {
     MONGO_SRV: 
-"mongodb+srv://NAtik:Nadin123@reactreserve-9yaic.mongodb.net/test?retryWrites=true&w=majority+srv://NAtik:Nadin123@reactreserve.kcuoy.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://NAtik:Nadin123@reactreserve-9yaic.mongodb.net/test?retryWrites=true&w=majority",
     JWT_SECRET: "<insert-jwt-secret>",
-    CLOUDINARY_URL: "<insert-cloudinary-url>",
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/ecommerce-application/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
   }
 };
